04eb1c5ead28ad9138239ffbe838cf755e1b55e4
Elias Benbourenane <eliasbenbourenane@gmail.com>
2022-05-22 17:13:46 +0400

    NEW: t&c + privacy policy

--- sidebars.js
+++ sidebars.js
@@ -2,28 +2,44 @@
 const sidebars = {
   docs: [
     {
-      type: 'doc',
-      label: 'Introduction',
-      id: 'intro',
+      type: "doc",
+      label: "Introduction",
+      id: "intro",
     },
     {
-      type: 'category',
-      label: 'Getting Started',
+      type: "category",
+      label: "Getting Started",
       items: [
         {
-          type: 'doc',
-          label: 'Local Deployement',
-          id: 'getting-started/local-deployement',
+          type: "doc",
+          label: "Local Deployement",
+          id: "getting-started/local-deployement",
         },
         {
-          type: 'doc',
-          label: 'Heroku Deployement',
-          id: 'getting-started/deployement-on-heroku',
+          type: "doc",
+          label: "Heroku Deployement",
+          id: "getting-started/deployement-on-heroku",
         },
         {
-          type: 'doc',
-          label: 'Dester Config',
-          id: 'getting-started/deployement-on-heroku',
+          type: "doc",
+          label: "Dester Config",
+          id: "getting-started/deployement-on-heroku",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Additional Info",
+      items: [
+        {
+          type: "doc",
+          label: "Terms and Conditions",
+          id: "additional-info/terms-and-conditions",
+        },
+        {
+          type: "doc",
+          label: "Privacy",
+          id: "additional-info/privacy-policy",
         },
       ],
     },
